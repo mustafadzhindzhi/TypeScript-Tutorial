86918dab6f4cdf1220403b32e6c7aad7474c1e99
mustafadzhindzhi <122883380+mustafadzhindzhi@users.noreply.github.com>
2023-12-22 17:13:28 +0200

    typescript

--- sandbox.js
+++ sandbox.js
@@ -1,13 +1,29 @@
-var character = 'mario';
-var age = 30;
-var isBlackBelt = false;
-// character = 20
-character = 'luigi';
-// age =  'yoush'
-age = 40;
-// isBlackBelt = 'yes';
-isBlackBelt = true;
-var circ = function (diameter) {
-    return diameter * Math.PI;
+//arrays 
+var names = ['luigi', 'mario', 'yoshi'];
+names.push('toad');
+// names.push(4);
+// names[0] = 3;
+var numbers = [10, 20, 30, 40];
+numbers.push(25);
+// numbers.push('shon');
+// numbers[1] = 'shon'
+var mixed = ['ken', 4, 'chun-li', 8, 9];
+mixed.push('ryu');
+mixed.push(3);
+mixed[1] = 1;
+//objects
+var ninja = {
+    name: 'Mario',
+    belt: 'black',
+    age: 30
 };
-console.log(circ(3));
+ninja.age = 40;
+ninja.name = 'Ryu';
+// ninja.age = 'ss'
+// ninja.skills = ['fighting, sneaking'];
+ninja = {
+    name: 'yoshi',
+    belt: 'orange',
+    age: 20,
+    // skills: ['sneaking']
+};
--- sandbox.ts
+++ sandbox.ts
@@ -1,18 +1,37 @@
-var character = 'mario';
-var age = 30;
-var isBlackBelt = false;
+//arrays 
+let names = ['luigi', 'mario', 'yoshi'];
 
-// character = 20
-character = 'luigi';
+names.push('toad');
+// names.push(4);
+// names[0] = 3;
 
-// age =  'yoush'
-age = 40;
+let numbers = [10, 20, 30, 40];
 
-// isBlackBelt = 'yes';
-isBlackBelt = true
+numbers.push(25);
+// numbers.push('shon');
+// numbers[1] = 'shon'
 
-const circ = (diameter: number) => {
-    return diameter * Math.PI;
+let mixed = ['ken', 4, 'chun-li', 8, 9];
+
+mixed.push('ryu');
+mixed.push(3);
+mixed[1] = 1;
+
+//objects
+let ninja = {
+    name:'Mario',
+    belt: 'black',
+    age: 30
 };
 
-console.log(circ(3));
+ninja.age = 40;
+ninja.name = 'Ryu';
+// ninja.age = 'ss'
+// ninja.skills = ['fighting, sneaking'];
+
+ninja = {
+    name: 'yoshi',
+    belt: 'orange',
+    age: 20,
+    // skills: ['sneaking']
+}
