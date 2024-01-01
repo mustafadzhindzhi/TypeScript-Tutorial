8933bbdc2aafb41b96428ba03a2cbb7e8723f6ea
mustafadzhindzhi <122883380+mustafadzhindzhi@users.noreply.github.com>
2024-01-01 16:59:26 +0200

    typescript

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,15 @@
 //classes
 class Invoice {
-    client: string;
-    details: string;
-    amount: number;
+    // readonly client: string;
+    // private details: string;
+    // public amount: number;
 
-    constructor(c: string, d:string, a: number) {
-        this.client = c;
-        this.details = d;
-        this.amount = a;
+    constructor(
+        readonly client: string, 
+        private details: string, 
+        public amount: number,
+    ) {
+        
     }
 
     format() {
@@ -22,13 +24,9 @@ let invoices: Invoice[] = [];
 invoices.push(invOne);
 invoices.push(invTwo);
 
-invOne.client = 'yoshi';
-invTwo.amount = 200;
-
-console.log(invOne, invTwo);
-
-console.log(invoices);
-
+invoices.forEach(inv => {
+    console.log(inv.client, inv.amount, inv.format());
+})
 
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 // console.log(form.children);
